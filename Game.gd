e7949c9a26dec9e3f444721821398946c7826774
Zak <zakthompson@fastmail.com>
2024-05-24 10:49:05 -0400

    Add connecting

--- Game.gd
+++ Game.gd
@@ -1,10 +1,31 @@
 extends Node2D
 
+var port = 6161
+var server = "0.0.0.0"
+
 var player
 
 func _ready():
 	randomize()
 	player = $GameMap/Link
+	var peer
+	if OS.get_name() == "HTML5":
+		peer = WebSocketClient.new()
+		connect_network_signals(peer)
+		peer.connect_to_url("%s:%s" % [server, port], [], true)
+		yield(peer, "connection_succeeded")
+	else:
+		peer = WebSocketServer.new()
+		connect_network_signals(peer)
+		peer.listen(port, [], true)
+	get_tree().network_peer = peer
+	player.name = str(get_tree().get_network_unique_id())
+	
+func connect_network_signals(peer: NetworkedMultiplayerPeer):
+	peer.connect("peer_connected", self, "_peer_connected")
+	
+func _peer_connected(id):
+	print("%s connected successfully" % [id])
 
 func _physics_process(_delta):
 	var direction = Vector2()
